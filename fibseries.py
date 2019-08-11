9d4dc743da075ea81c04d801c28bceabbbfdd442
Keerthana.T <48148816+keerthana0110@users.noreply.github.com>
2019-08-12 00:00:48 +0530

    Create fibseries.py

---
+++ fibseries.py
@@ -0,0 +1,13 @@
+def fib():
+   n = int(input("Please Enter a number: "))
+
+   if n == 1:
+      return(1)
+   elif n == 0:   
+      return(0)            
+   else:                      
+      return (n-1) + (n-2)
+
+
+mylist = range[0:n]
+print(mylist)
